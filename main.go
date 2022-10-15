b20b874a80fdad520c4b86c3ac37d7a855fae478
Alex <thealexsb@gmail.com>
2022-10-15 21:57:12 +0300

    read standalone external battery status via tcp serial

--- main.go
+++ main.go
@@ -1,76 +1,201 @@
 package main
 
 import (
-	"github.com/aprosvetova/ninebot-mqtt/mqtt"
-	"github.com/aprosvetova/ninebot-mqtt/scooter"
-	"github.com/currantlabs/ble"
-	"github.com/currantlabs/ble/linux"
+	"bufio"
+	"errors"
+	"fmt"
+	"github.com/aprosvetova/ninebot-mqtt/scooter/protocol"
 	"log"
+	"net"
+	"os"
+	"time"
 )
 
-var mqttC *mqtt.Client
-var connected = make(map[string]struct{})
-var config *Config
-
 func main() {
-	var err error
-	config, err = getConfig()
-	if err != nil {
-		log.Printf("can't load config file, DEFAULT VALUES will be used: %s\n", err)
-	}
+	processSerial()
+}
 
-	d, err := linux.NewDevice()
-	if err != nil {
-		log.Fatalf("can't create BLE device: %s\n", err)
-	}
-	ble.SetDefaultDevice(d)
+var addr string
 
-	mqttC, err = mqtt.Connect(mqtt.Options{
-		Address: config.Address,
-		Username: config.Username,
-		Password: config.Password,
-		BatteryTopic: config.BatteryTopic,
-		AvailabilityTopic: config.AvailabilityTopic,
-		PayloadAvailable: config.PayloadAvailable,
-		PayloadNotAvailable: config.PayloadNotAvailable,
-	})
-	if err != nil {
-		log.Fatalf("can't connect to MQTT: %s\n", err)
-	}
-	log.Println("connected to MQTT")
-
-	log.Println("started listening")
+func processSerial() {
+	addr = "192.168.88.82:1234"
+	checkConnection(true)
 	for {
-		scooters, err := scooter.FindScooters()
-		if err.Error() == "context canceled" {
-			log.Fatalln("quitting...")
+		statusReq := protocol.GetStatus()
+		statusResp, err := request(statusReq)
+		if err != nil {
+			log.Fatalf("status request error: %s", err.Error())
 		}
-		for _, mac := range scooters {
-			if !containsKey(connected, mac) && !containsKey(config.ignoredScootersMap, mac) {
-				go connect(mac)
+		status := statusResp.Payload[0]
+
+		serialReq := protocol.GetSerial()
+		serialResp, err := request(serialReq)
+		if err != nil {
+			log.Fatalf("serial request error: %s", err.Error())
+		}
+		serial := serialResp.Payload
+
+		remainingCapacityPercReq := protocol.GetRemainingCapacityPerc()
+		remainingCapacityPercResp, err := request(remainingCapacityPercReq)
+		if err != nil {
+			log.Fatalf("remainingCapacityPerc request error: %s", err.Error())
+		}
+		remainingCapacityPerc := remainingCapacityPercResp.Payload
+
+		remainingCapacityReq := protocol.GetRemainingCapacity()
+		remainingCapacityResp, err := request(remainingCapacityReq)
+		if err != nil {
+			log.Fatalf("remainingCapacity request error: %s", err.Error())
+		}
+		remainingCapacity := remainingCapacityResp.Payload
+
+		currentReq := protocol.GetCurrent()
+		currentResp, err := request(currentReq)
+		if err != nil {
+			log.Fatalf("current request error: %s", err.Error())
+		}
+		current := currentResp.Payload
+
+		voltageReq := protocol.GetVoltage()
+		voltageResp, err := request(voltageReq)
+		if err != nil {
+			log.Fatalf("voltage request error: %s", err.Error())
+		}
+		voltage := voltageResp.Payload
+
+		temperatureReq := protocol.GetTemperature()
+		temperatureResp, err := request(temperatureReq)
+		if err != nil {
+			log.Fatalf("temperature request error: %s", err.Error())
+		}
+		temperature := temperatureResp.Payload
+
+		log.Printf(
+			"[%s] [%d%% / %dmAh] status: %x; %.2f A, %.2f V; %d°/%d°",
+			serial,
+			toInt(remainingCapacityPerc),
+			toInt(remainingCapacity),
+			status,
+			float64(toInt(current))*10/1000,
+			float64(toInt(voltage))*10/1000,
+			int(temperature[0])-20,
+			int(temperature[1])-20)
+	}
+
+}
+
+func request(req []byte) (*protocol.Response, error) {
+	var parsed *protocol.Response
+	for {
+		checkConnection(false)
+		//printBytes("status request", req)
+		nBytes, errWrite := fmt.Fprintf(c, string(req))
+		if errors.Is(errWrite, os.ErrDeadlineExceeded) {
+			log.Printf("timeout writing to socket (%d bytes written): %s", nBytes, errWrite.Error())
+			checkConnection(true)
+			continue
+		} else if errWrite != nil {
+			log.Fatalf("error writing to socket (%d bytes written): %s", nBytes, errWrite.Error())
+		}
+
+		response, readErr := waitResponse(r)
+		if errors.Is(readErr, os.ErrDeadlineExceeded) {
+			log.Printf("timeout reading socket: %s", readErr.Error())
+			checkConnection(true)
+			continue
+		} else if readErr != nil {
+			log.Fatalf("error reading socket: %s", readErr.Error())
+		}
+
+		//printBytes("status response", response)
+		var parseErr error
+		parsed, parseErr = protocol.ParseResponse(response)
+		if parseErr != nil {
+			if parseErr.Error() == "wrong checksum" {
+				log.Printf("parse error: %s", parseErr.Error())
+				continue
 			}
+			log.Fatalf("parse error: %s", parseErr.Error())
+		}
+		//printBytes("resp parsed", parsed.Payload)
+
+		return parsed, nil
+	}
+}
+
+var c net.Conn
+var r *bufio.Reader
+
+func checkConnection(force bool) {
+	var err error
+	if force || c == nil {
+		if c != nil {
+			c.Close()
+		}
+		log.Printf("Connecting...")
+		c, err = net.Dial("tcp", addr)
+		if err != nil {
+			log.Fatalf("error connecting to socket: %s", err.Error())
+		}
+		r = bufio.NewReader(c)
+		log.Printf("Connected to %s", addr)
+	}
+	c.SetDeadline(time.Now().Add(2000 * time.Millisecond))
+}
+
+func waitResponse(reader *bufio.Reader) ([]byte, error) {
+	buf := make([]byte, 0)
+	var awaitedLen int
+	for {
+		oneByte, err := reader.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		buf = append(buf, oneByte)
+		if len(buf) == 3 {
+			awaitedLen = int(buf[2])
+		} else if awaitedLen != 0 && len(buf) == awaitedLen+9 {
+			return buf, nil
 		}
 	}
 }
 
-func connect(mac string) {
-	connected[mac] = struct{}{}
-	name := scooter.Listen(mac, handleBattery)
-	if config.ScooterNaming == "mac" {
-		name = mac
+func waitResponseWithTimeout(reader *bufio.Reader, timeoutMs time.Duration) ([]byte, error) {
+	c1 := make(chan []byte, 1)
+	c2 := make(chan error, 1)
+	go func() {
+		bytes, err := waitResponse(reader)
+		if err != nil {
+			c2 <- err
+		}
+		c1 <- bytes
+	}()
+
+	select {
+	case res := <-c1:
+		return res, nil
+	case err := <-c2:
+		return nil, err
+	case <-time.After(timeoutMs * time.Millisecond):
+		return nil, errors.New("timeout")
 	}
-	mqttC.SendOffline(name)
-	delete(connected, mac)
 }
 
-func handleBattery(name string, mac string, percent int) {
-	if config.ScooterNaming == "mac" {
-		name = mac
-	}
-	mqttC.SendBatteryStatus(name, percent)
+func printBytes(tag string, bytes []byte) {
+	log.Printf("%s (%d):\t %x \t %# x", tag, len(bytes), bytes, bytes)
 }
 
-func containsKey(m map[string]struct{}, v string) bool {
-	_, contains := m[v]
-	return contains
+func toInt(bytes []byte) int {
+	result := 0
+	//for i := 0; i < len(bytes); i++ {
+	//	result = result << 8
+	//	result += int(bytes[i])
+	//}
+	//REVERSE ORDER!!!!
+	for i := len(bytes) - 1; i >= 0; i-- {
+		result = result << 8
+		result += int(bytes[i])
+	}
+
+	return result
 }
--- scooter/protocol/protocol.go
+++ scooter/protocol/protocol.go
@@ -6,14 +6,43 @@ import (
 	"errors"
 )
 
-const ReadRequestCommand RequestCommand = 0x01
+const ReadRegisterCommand RequestCommand = 0x01
 
 func GetBattery() []byte {
-	return CreateRequest(ReadRequestCommand, 0x22, 0x02)
+	return CreateRequest(ReadRegisterCommand, 0x10, 0x02)
 }
 
-func CreateRequest(command RequestCommand, param byte, payload... byte) []byte {
-	cmd := []byte{0x5A, 0xA5, 0x00, 0x3E, 0x20, byte(command), param}
+func GetStatus() []byte {
+	//return CreateRequest(ReadRegisterCommand, 0x30, 0x0C)
+	return CreateRequest(ReadRegisterCommand, 0x30, 0x02)
+}
+
+func GetSerial() []byte {
+	return CreateRequest(ReadRegisterCommand, 0x10, 0x0E)
+}
+
+func GetRemainingCapacityPerc() []byte {
+	return CreateRequest(ReadRegisterCommand, 0x32, 0x02)
+}
+
+func GetRemainingCapacity() []byte {
+	return CreateRequest(ReadRegisterCommand, 0x31, 0x02)
+}
+
+func GetCurrent() []byte {
+	return CreateRequest(ReadRegisterCommand, 0x33, 0x02)
+}
+
+func GetVoltage() []byte {
+	return CreateRequest(ReadRegisterCommand, 0x34, 0x02)
+}
+
+func GetTemperature() []byte {
+	return CreateRequest(ReadRegisterCommand, 0x35, 0x02)
+}
+
+func CreateRequest(command RequestCommand, param byte, payload ...byte) []byte {
+	cmd := []byte{0x5A, 0xA5, 0x00, 0x20, 0x22, byte(command), param}
 	cmd = append(cmd, payload...)
 	cmd[2] = byte(len(payload))
 	cmd = append(cmd, getChecksum(cmd[2:])...)
@@ -24,19 +53,19 @@ func ParseResponse(raw []byte) (*Response, error) {
 	if len(raw) < 9 {
 		return nil, errors.New("raw is too short")
 	}
-	if raw[0] != 0x5A || raw[1] != 0xA5 || raw[3] != 0x20 || raw[4] != 0x3E {
+	if raw[0] != 0x5A || raw[1] != 0xA5 {
 		return nil, errors.New("not a Ninebot ES raw")
 	}
-	if raw[2] != uint8(len(raw) - 9) {
+	if raw[2] != uint8(len(raw)-9) {
 		return nil, errors.New("wrong payload length byte")
 	}
 	if !bytes.Equal(raw[len(raw)-2:], getChecksum(raw[2:len(raw)-2])) {
 		return nil, errors.New("wrong checksum")
 	}
 	response := &Response{
-		Command: raw[5],
+		Command:   raw[5],
 		Parameter: raw[6],
-		Payload: raw[7:len(raw)-2],
+		Payload:   raw[7 : len(raw)-2],
 	}
 	return response, nil
 }
